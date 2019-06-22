200eb872929f1f12226b1811c39c0e57699a5a58
liyuxing <yuxing.li@ims.jsfund.cn>
2019-06-22 18:08:39 +0800

    add common:param config model

--- common/models.py
+++ common/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ParamConfig(models.Model):
+    '''键值配置
+    Key-Value config
+    '''
+    key = models.CharField(primary_key=True, max_length=100)
+    value = models.CharField(max_length=100)
+    remark = models.CharField(max_length=100)
+    created = models.DateTimeField('config created')
