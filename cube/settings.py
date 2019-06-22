1d0d1fdc219f12fe6e29e0e5228338834fef567a
liyuxing <yuxing.li@ims.jsfund.cn>
2019-06-22 12:31:25 +0800

    add heroku config

--- cube/settings.py
+++ cube/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'wde)kxwrw47*6oj48be*%0i%772_*b&w$*!_-($zk)bsdz4y1#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-cube.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
