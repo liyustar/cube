c876dea9f8067cf31ad106dbb5f758679d5fb074
liyuxing <yuxing.li@ims.jsfund.cn>
2019-06-23 10:21:47 +0800

    add user permission

--- common/apis.py
+++ common/apis.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from common.models import ParamConfig
+from common.permissions import IsAdminUserOrReadOnly
 from common.serializers import UserSerializer, ParamConfigSerializer
 
 
@@ -11,6 +13,8 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
+    # permission_classes = (permissions.IsAdminUser,)
 
 
 class ParamConfigViewSet(viewsets.ModelViewSet):
